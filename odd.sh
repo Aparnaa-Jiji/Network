77f4bee4629fcbe6824c4c4bd06dbd3480069efe
Aparnaa-Jiji <aparnajiji003@gmail.com>
2025-03-28 14:00:24 +0530

    Add files via upload

--- odd.sh
+++ odd.sh
@@ -1,11 +1,14 @@
-check_odd_even()
-{
-if [ $((num%2)) -eq 0 ]; then
-	echo "$num is even"
-else
-	echo "$num is odd"
-fi
-}
-echo "Enter a number:"
-read num
-check_odd_even $num
+echo "Enter a set of numbers separated by spaces:"
+read -a numbers
+sum_even=0
+sum_odd=0
+for num in "${numbers[@]}"
+do
+	if [ $((num % 2)) -eq 0 ]; then
+		sum_even=$((sum_even + num))
+	else
+		sum_odd=$((sum_odd + num))
+	fi
+done
+echo "Sum of even numbers: $sum_even"
+echo "Sum of odd numbers: $sum_odd"
---
+++ palin.sh
@@ -0,0 +1,9 @@
+echo "Enter the string:"
+read s
+echo $s>temp
+rvs="$(rev temp)"
+if [ $s=$rvs ];then
+echo "Palindrome"
+else
+echo "Not a Palindrome"
+fi
