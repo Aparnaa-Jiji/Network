3039044f5feb5521e8a8650076a163a7d03a09da
Aparnaa-Jiji <aparnajiji003@gmail.com>
2025-05-02 15:39:53 +0530

    Add files via upload

---
+++ Program31.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#set the search directory
+search_dir="."
+#Find all ".txt" files  in the search directory and its subdirectories
+files=$(find "$search_dir" -type f -name "*.txt")
+#Initialize the count
+count=0
+#Loop through each file and count the occurences of "LInux"
+for file in $files; do
+   occurrences=$(grep -o "Linux" "$file")
+   count=$((count + occurrences))
+done
+#Print the total count
+echo "Total count of 'Linux' in all .txt files:$count"  
